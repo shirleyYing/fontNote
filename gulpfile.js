81471afdb15fef68a6a1d2919d957f24078e0100
shirley <646667294@qq.com>
2015-10-06 17:03:32 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 var gulp = require("gulp"),  
-  deploy = require("gulp-gh-pages");
+    deploy = require("gulp-gh-pages");
 
+var options={remoteUrl: "git@github.com:shirleyYing/fontNote_book.git"};
 gulp.task('publish', function () {  
   gulp.src("./_book/**/*")
-    .pipe(deploy({
-      remoteUrl: "git@github.com:shirleyYing/fontNote_book.git"
-    }))
+    .pipe(deploy(options))
     .on("error", function(err){
       console.log(err);
     });
