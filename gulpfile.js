8e26eccc3db3571b257260c37276ef41602c6b2e
shirley <646667294@qq.com>
2015-10-06 19:03:52 +0800

    rename repo

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require("gulp"),
     deploy = require("gulp-gh-pages");
 
 gulp.task('publish', function () {
-  gulp.src("book/**/*.*")
+  gulp.src("_book/**/*.*")
     .pipe(deploy({
-      remoteUrl: "git@github.com:shirleyYing/fontNote_book.git"
+      remoteUrl: "git@github.com:shirleyYing/fontNote.git"
     }))
     .on("error", function(err){
       console.log(err);
